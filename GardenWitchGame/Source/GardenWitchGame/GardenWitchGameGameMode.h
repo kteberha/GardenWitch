14594b25d28533adcfc8e2c1b882b8cc6c10ec8a
Kaleb Eberhart <kalebeberhart@gmail.com>
2020-08-29 16:02:54 -0500

    Basic Project

---
+++ GardenWitchGame/Source/GardenWitchGame/GardenWitchGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GardenWitchGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGardenWitchGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGardenWitchGameGameMode();
+};
+
+
+
